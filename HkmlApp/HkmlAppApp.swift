3d230b88bdbe18db7857b2f2f7b17fdcdd76ca2d
BROTHERSEVEN <brotherseven@gmail.com>
2023-08-29 18:28:54 +0800

    Initial Commit

---
+++ HkmlApp/HkmlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HkmlAppApp.swift
+//  HkmlApp
+//
+//  Created by Richard So on 29/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct HkmlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
